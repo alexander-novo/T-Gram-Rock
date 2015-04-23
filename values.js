6b54e4e0257596cdd2d5c50eb0f10b41ae70ff3c
CoreyPena <CoreyPena@gmail.com>
2015-04-22 19:34:42 -0700

    ID videos.

--- values.js
+++ values.js
@@ -15,7 +15,49 @@ var values = {
       }
     },
     "Corey Pena": {
-      email: "CoreyPena@gmail.com"
+      email: "CoreyPena@gmail.com",
+      videos: {
+        "Planning": {
+          source: "Corey Planning.mp4",
+          type: "video/mp4",
+          thumb: "CoreyPlanning.jpg",
+          title: "Planning a heist",
+          original: "I don't know.",
+          description: "Kids plan the ultimate heist of test answers."
+        },
+        "Execution": {
+          source: "Corey Execution.mp4",
+          type: "video/mp4",
+          thumb: "CoreyExecution.png",
+          title: "Bustin' Makes Me Feel Good.",
+          original: "GhostBusters",
+          description: "A flawlessly executed plan dealing with extraordinary subject matter."
+        },
+        "Risk Management": {
+          source: "Corey Risk Management.mp4",
+          type: "video/mp4",
+          thumb: "CoreyRisKManagement.jpg",
+          title: "Dr. Manhattan fixes a situation.",
+          original: "Watchmen",
+          description: "Dr Manhattan manages a risky loose end."
+        },
+        "Quality Control": {
+          source: "Corey QualityControl.mp4",
+          type: "video/mp4",
+          thumb: "CoreyQualityControl.jpg",
+          title: "Tony Stark's routine test.",
+          original: "Iron Man",
+          description: "Tony Stark tests his armor."
+        },
+        "Communications": {
+          source: "Corey Communication.mp4",
+          type: "video/mp4",
+          thumb: "CoreyCommunication.png",
+          title: "ET calls collect",
+          original: "ET",
+          description: "ET communicates home."
+        }
+      }
     },
     "Sharon Phillips": {
       email: "Sharonaphillips61@gmail.com",
